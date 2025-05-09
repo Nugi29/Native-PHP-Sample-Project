e9dbbfd06633f09e8042c75eda03e720a4e76218
Nugitha Disas <nugitha.c@gmail.com>
2025-05-09 12:04:10 +0530

    v2

---
+++ serverapp/gendercontroller.php
@@ -0,0 +1,23 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "1234";
+$database = "ems";
+
+$dbconn = new mysqli($servername, $username, $password, $database);
+
+if (!$dbconn) {
+    die("Connection failed: " . $dbconn->connect_error);
+}
+
+$genders = array();
+
+$sql = "SELECT * FROM gender";
+$result = $dbconn->query($sql);
+
+while ($row = mysqli_fetch_assoc($result)) {
+    array_push($genders, $row);
+}
+
+echo json_encode($genders);
